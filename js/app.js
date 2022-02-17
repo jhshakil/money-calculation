6311c0641c63448141825ee64ea932d49866927e
Shakil <shakil11271@gmail.com>
2022-02-17 15:29:54 +0600

    update-5

--- js/app.js
+++ js/app.js
@@ -8,15 +8,6 @@ function input(names) {
 }
 
 function cost() {
-    // const foodInput = document.getElementById('food-input').value;
-    // const rentInput = document.getElementById('rent-input').value;
-    // const clothesInput = document.getElementById('clothes-input').value;
-    // if (foodInput <= 0 && rentInput <= 0 && clothesInput <= 0) {
-    //     const costCalculate = parseInt(foodInput) + parseInt(rentInput) + parseInt(clothesInput);
-    //     return costCalculate;
-    // } else {
-    //     alert('Please enter a valid number');
-    // }
     const foodInput = parseInt(input('food'));
     const rentInput = parseInt(input('rent'));
     const clothesInput = parseInt(input('clothes'));
@@ -32,13 +23,6 @@ function cost() {
     }
 }
 
-// function incomeRemain() {
-//     const incomeInput = document.getElementById('income-input');
-//     const costSubTotal = cost();
-//     const balanceCheck = parseInt(incomeInput.value) - costSubTotal;
-//     return balanceCheck;
-// }
-
 function calculation() {
     const incomeInput = input('income');
     const incomeInteger = parseInt(incomeInput);
@@ -50,43 +34,43 @@ function calculation() {
     const totalCosting = document.getElementById('total-cost');
     const totalCostingInner = totalCosting.innerText;
     const costSubTotal = cost();
-    if (incomeInteger >= costSubTotal) {
-        if (costSubTotal != undefined) {
-            costing.innerText = costSubTotal;
-            if (isNaN(incomeInteger)) {
-                totalCosting.innerText = totalCostingInner;
-            } else {
-                const balance = incomeInput - costSubTotal;
-                totalCosting.innerText = balance;
-            }
-        } else {
-            costing.innerText = costingInner;
+    if (incomeInteger < costSubTotal) {
+        return alert('Please enter a valid number');
+    }
+
+    if (costSubTotal != undefined) {
+        costing.innerText = costSubTotal;
+        if (isNaN(incomeInteger)) {
             totalCosting.innerText = totalCostingInner;
+        } else {
+            const balance = incomeInput - costSubTotal;
+            totalCosting.innerText = balance;
         }
     } else {
-        alert('Please enter a valid number');
+        costing.innerText = costingInner;
+        totalCosting.innerText = totalCostingInner;
     }
-    // const balance = parseInt(incomeInput) - costSubTotal;
-    // console.log(typeof balance);
-    // // totalCosting.innerText = balance;
-    // if (balance != NaN) {
-    //     totalCosting.innerText = balance;
-    // } else {
-    //     totalCosting.innerText = totalCostingInner;
-    // }
 }
 
 function saving() {
-    const incomeInput = input('income');
-    const savingInput = document.getElementById('saving-input');
+    const incomeInput = parseInt(input('income'));
+    const savingInput = parseInt(input('saving'));
+    if (isNaN(savingInput)) {
+        return alert('Please enter a valid number');
+    }
     const savingAmount = document.getElementById('saving-amount');
-    const persent = parseInt(savingInput.value) / 100;
+    const persent = savingInput / 100;
     const savingBalence = parseInt(incomeInput) * persent;
     savingAmount.innerText = savingBalence;
 
     // remaining balance 
     const remainingBalance = document.getElementById('remaining-balance');
     const costSubTotal = cost();
-    const checkBalance = parseInt(incomeInput) - costSubTotal - savingBalence;
-    remainingBalance.innerText = checkBalance;
+    const balance = incomeInput - costSubTotal;
+    if (savingBalence > balance) {
+        return alert('You do not have much money')
+    } else {
+        const checkBalance = balance - savingBalence;
+        remainingBalance.innerText = checkBalance;
+    }
 }
